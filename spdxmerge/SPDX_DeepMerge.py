40cc3518a1bb864e20e552479a38b1f088882ea2
Adinath Mhatre <adinath.mhatre@philips.com>
2023-10-11 20:02:33 +0530

    SPDX_DeepMerge: Skip duplicate relationships

--- spdxmerge/SPDX_DeepMerge.py
+++ spdxmerge/SPDX_DeepMerge.py
@@ -6,6 +6,8 @@ from spdx_tools.spdx.model import (
     ActorType,
     CreationInfo,
     Document,
+    Relationship,
+    RelationshipType,
 )
 
 class SPDX_DeepMerger():
@@ -31,10 +33,18 @@ class SPDX_DeepMerger():
         master_doc = Document(creation_info)
 
         master_doc.packages = []
+        master_doc.relationships = []
         for doc in self.doc_list:
             for doc_package in doc.packages:
                 if doc_package not in master_doc.packages:
                     master_doc.packages.extend([doc_package])
+                    # Add document relationship with package
+                    doc_relationship = Relationship(
+                        spdx_element_id=DOCUMENT_SPDX_ID,
+                        relationship_type=RelationshipType.DESCRIBES,
+                        related_spdx_element_id=doc_package.spdx_id,
+                    )
+                    master_doc.relationships.append(doc_relationship)
 
         master_doc.files = []
         for doc in self.doc_list:
@@ -48,9 +58,10 @@ class SPDX_DeepMerger():
         for doc in self.doc_list:
             master_doc.extracted_licensing_info.extend(doc.extracted_licensing_info)
 
-        master_doc.relationships = []
         for doc in self.doc_list:
-            master_doc.relationships.extend(doc.relationships)
+            for new_rel in doc.relationships:
+                if new_rel not in master_doc.relationships:
+                    master_doc.relationships.append(new_rel)
 
         master_doc.annotations = []
         for doc in self.doc_list:
